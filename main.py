e05ac7869625452672d650e93985716329b8a1a0
dominikmatejcek <dominik.matejcek@techtitans.cz>
2024-12-11 10:35:21 +0100

    le app

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+# app.py
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from django.http import HttpResponse
+from django.conf import settings
+from django.urls import path
+from django.core.wsgi import get_wsgi_application
 
+# Install Django before running:
+# pip install django
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Create a basic Django settings structure
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='randomsecretkey',  # Make sure to change it for production
+    ROOT_URLCONF=__name__,
+    ALLOWED_HOSTS=['*'],
+    MIDDLEWARE=['django.middleware.common.CommonMiddleware'],
+    INSTALLED_APPS=['django.contrib.contenttypes'],
+)
 
+# Define a simple view
+def home(request):
+    return HttpResponse("Hello, this is my simple Django app!")
 
-# Press the green button in the gutter to run the script.
+# URL patterns for the app
+urlpatterns = [
+    path('', home, name='home'),
+]
+
+# Set the WSGI application to allow Django to run as a web app
+application = get_wsgi_application()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Django's default development server (useful for local testing)
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver'])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
